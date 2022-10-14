2ac1948d6c53bcd66b35ecc44574c8d80da5c4b0
Philippe Matray <phmatray@gmail.com>
2022-10-14 11:40:57 +0200

    Add DemoWebsite

--- src/DemoApi/Program.cs
+++ src/DemoApi/Program.cs
@@ -1,17 +1,31 @@
 using DemoApi;
 using DemoApi.Database;
 
+const string myAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
 var config = builder.Configuration;
+
+services.AddCors(options =>
+{
+    options.AddPolicy(name: myAllowSpecificOrigins,
+        policy =>
+        {
+            policy.WithOrigins("https://localhost:7270");
+        });
+});
 
 services.AddNinjadog(config);
 
 var app = builder.Build();
 
+app.UseCors(myAllowSpecificOrigins);
 app.UseNinjadog();
 
-var databaseInitializer = app.Services.GetRequiredService<DatabaseInitializer>();
-await databaseInitializer.InitializeAsync();
+await app.Services
+    .GetRequiredService<DatabaseInitializer>()
+    .InitializeAsync()
+    .ConfigureAwait(false);
 
 app.Run();
